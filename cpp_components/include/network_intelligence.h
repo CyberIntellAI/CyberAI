bdeec91ee662726de958b26ed19dd243e5035c49
CyberIntellAI <bocacountryclubhelp@gmail.com>
2025-01-04 13:57:31 -0500

    Update network_intelligence.h

--- cpp_components/include/network_intelligence.h
+++ cpp_components/include/network_intelligence.h
@@ -1,1 +1,16 @@
+#ifndef NETWORK_INTELLIGENCE_H
+#define NETWORK_INTELLIGENCE_H
 
+#include <string>
+#include <vector>
+
+class NetworkIntelligence {
+public:
+    NetworkIntelligence();
+    void analyzeTraffic(const std::vector<std::string>& data);
+    std::vector<std::string> getInsights() const;
+private:
+    std::vector<std::string> insights;
+};
+
+#endif // NETWORK_INTELLIGENCE_H
